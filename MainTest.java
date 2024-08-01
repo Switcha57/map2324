26d26e45f0ab71181e191dd623dab8e6bae17231
Losurdo Mauro <losurdo57@gmail.com>
2024-08-01 20:57:35 +0200

    completato LAB2

--- MainTest.java
+++ MainTest.java
@@ -1,6 +1,9 @@
 import data.*;
 import clustering.*;
 import distance.*;
+
+import java.util.InputMismatchException;
+
 public class MainTest {
 
     /**
@@ -10,31 +13,56 @@ public class MainTest {
 
         Data data =new Data();
         System.out.println(data);
-        System.out.print("Inserire la profondità del dendrogramma: ");
-        int k = Keyboard.readInt(); //5
-        HierachicalClusterMiner clustering=new HierachicalClusterMiner(k);
+        HierachicalClusterMiner clustering=null;
+        boolean invalidInput = false;
+        do {
+            invalidInput= false;
+            System.out.print("Inserire la profondità del dendrogramma: ");
+            int k = Keyboard.readInt();//5
+            try{
+                clustering=new HierachicalClusterMiner(k);
+            }catch (InvalidDepthException e){
+                System.out.println(e);
+                invalidInput = true;
+            }
 
-        System.out.println("Single link distance");
-        ClusterDistance distance=new SingleLinkDistance();
+        }while(invalidInput);
 
-        double [][] distancematrix=data.distance();
-        System.out.println("Distance matrix:\n");
-        for(int i=0;i<distancematrix.length;i++) {
-            for(int j=0;j<distancematrix.length;j++)
-                System.out.print(distancematrix[i][j]+"\t");
-            System.out.println("");
+        int choice = 0;
+        do{
+            System.out.println("Che distanza vuoi:\n 1) Single Link Distance\n2) Average Link Distance");
+            choice = Keyboard.readInt();
+            if (choice != 1 && choice != 2){
+                invalidInput = true;
+            }else {
+                invalidInput=false;
+            }
+            }while(invalidInput);
+        switch(choice) {
+            case 1:
+                System.out.println("Single link distance");
+                ClusterDistance distance=new SingleLinkDistance();
+
+                double [][] distancematrix=data.distance();
+                System.out.println("Distance matrix:\n");
+                for(int i=0;i<distancematrix.length;i++) {
+                    for(int j=0;j<distancematrix.length;j++)
+                        System.out.print(distancematrix[i][j]+"\t");
+                    System.out.println("");
+                }
+
+                clustering.mine(data,distance);
+                System.out.println(clustering);
+                System.out.println(clustering.toString(data));
+            case 2:
+                System.out.println("Average link distance");
+                distance=new AverageLinkDistance();
+                clustering.mine(data,distance);
+                System.out.println(clustering);
+                System.out.println(clustering.toString(data));
         }
 
-        clustering.mine(data,distance);
-        System.out.println(clustering);
-        System.out.println(clustering.toString(data));
 
-
-        System.out.println("Average link distance");
-        distance=new AverageLinkDistance();
-        clustering.mine(data,distance);
-        System.out.println(clustering);
-        System.out.println(clustering.toString(data));
 
     }
 
