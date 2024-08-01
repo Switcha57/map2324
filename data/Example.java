54e09656c1cf83c63e34f54879c97a568d189e5d
pietrogad <gadapie.03@gmail.com>
2024-08-01 22:06:00 +0200

    fix: modificato la classe Example.

--- data/Example.java
+++ data/Example.java
@@ -1,57 +1,43 @@
 package data;
-import java.util.Arrays;
+import java.util.*;
+
 import static java.lang.Math.pow;
 
-public class Example {
-    private int length=0;
-    private Double [] example;
+public class Example implements Iterable<Double>{
+    private List<Double> example;
 
-    Example(int length)throws IllegalArgumentException{
-        if(length<0){
-            throw new IllegalArgumentException("Lunghezza deve essere maggiore di zero");
-        }else{
-            this.length = length;
-            example = new Double[length];
-        }
-    }
     Example(){
-        example = new Double[length];
+        example = new LinkedList<>();
     }
+    public Iterator<Double> iterator() {
+        return example.iterator();
+    }
+
     double getExample(int index) throws ArrayIndexOutOfBoundsException{
-        if(index<0 || index>length-1){
-            throw new ArrayIndexOutOfBoundsException("Indice deve essere compreso tra 0 e " + (length-1));
+        if(index<0 || index>example.size()-1){
+            throw new ArrayIndexOutOfBoundsException("Indice deve essere compreso tra 0 e " + (example.size()-1));
         }else{
-            return example[index];
+            return example.get(index);
         }
 
     }
-    int getLength(){
-        return length;
-    }
-    void setExample(int index , double v) throws ArrayIndexOutOfBoundsException{
-        if(index<0 || index>length-1){
-            throw new ArrayIndexOutOfBoundsException("Indice deve essere compreso tra 0 e " + (length-1));
-        }else{
-            example[index] = v;
-        }
+    void addExample(double v) {
+        example.add(v);
 
     }
-    void setLength(int length) throws IllegalArgumentException {
-        if(length<0){
-            throw new IllegalArgumentException("Lunghezza deve essere maggiore di zero");
-        }else{
-            this.length = length;
-        }
 
+    int getSize(){
+        return example.size();
     }
 
     public double distance(Example newE) throws InvalidSizeException {
         int d=0;
-        if(this.length == newE.getLength()){
-            for(int i=0; i<length; i++){
-                d+= pow(this.example[i]-newE.getExample(i),2);
+        Iterator<Double> it = newE.iterator();
+        Iterator<Double> it2 = example.iterator();
+        if(this.getSize() == newE.getSize()){
+            while(it.hasNext()){
+                d+=pow(it.next()-it2.next(),2.0);
             }
-
         }else{
             throw new InvalidSizeException("Dimensione dei due array diversa");
         }
@@ -59,6 +45,6 @@ public class Example {
     }
 
     public String toString(){
-        return Arrays.toString(example);
+        return Arrays.toString(example.toArray());
     }
 }
